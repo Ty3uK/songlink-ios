5cf02e27bbb372279ae9e950980d1ccaf56c6ac1
Maksim Karelov <max.karelov@gmail.com>
2019-03-05 12:41:45 +0300

    feat: new release
    Version 1.1
    - Now support new song.link data format

--- share/SongLink.swift
+++ share/SongLink.swift
@@ -2,23 +2,23 @@ import Foundation
 import RxSwift
 
 let BASE_URL = "https://song.link/"
-let INITIAL_STATE_REGEXP = "<script id=\"initialState\".+>(.+)</script>"
+let INITIAL_STATE_REGEXP = "<script id=\"initialState\".+>\\s+?(.+)\\s+?</script>"
 let SERVICES = [
-    SLProvider(name: "yandex", label: "Yandex Music", url: ""),
-    SLProvider(name: "google", label: "Google Music", url: ""),
-    SLProvider(name: "appleMusic", label: "Apple Music", url: ""),
-    SLProvider(name: "spotify", label: "Spotify", url: ""),
-    SLProvider(name: "youtube", label: "Youtube", url: ""),
-    SLProvider(name: "youtubeMusic", label: "Youtube Music", url: ""),
-    SLProvider(name: "deezer", label: "Deezer", url: ""),
-    SLProvider(name: "pandora", label: "Pandora", url: ""),
-    SLProvider(name: "soundcloud", label: "SoundCloud", url: ""),
-    SLProvider(name: "tidal", label: "Tidal", url: ""),
-    SLProvider(name: "songlink", label: "song.link", url: "")
+    SLProvider(name: "YANDEX_SONG", label: "Yandex Music", url: ""),
+    SLProvider(name: "GOOGLE_SONG", label: "Google Music", url: ""),
+    SLProvider(name: "ITUNES_SONG", label: "Apple Music", url: ""),
+    SLProvider(name: "SPOTIFY_SONG", label: "Spotify", url: ""),
+    SLProvider(name: "YOUTUBE_VIDEO", label: "Youtube", url: ""),
+    SLProvider(name: "YOUTUBE_SONG", label: "Youtube Music", url: ""),
+    SLProvider(name: "DEEZER_SONG", label: "Deezer", url: ""),
+    SLProvider(name: "PANDORA_SONG", label: "Pandora", url: ""),
+    SLProvider(name: "SOUNDCLOUD_SONG", label: "SoundCloud", url: ""),
+    SLProvider(name: "TIDAL_SONG", label: "Tidal", url: ""),
+    SLProvider(name: "SONGLINK", label: "song.link", url: "")
 ]
 
 struct SL: Decodable {
-    let songlink: SLData
+    var songlink: SLData
 }
 
 struct SLResponse {
@@ -29,11 +29,29 @@ struct SLResponse {
 struct SLData: Decodable {
     let title: String
     let artistName: String
-    let links: SLLinks
-}
+    let nodesByUniqueId: [String: SLNode]
 
-struct SLLinks: Decodable {
-    let listen: [SLLink]
+    var links: [SLLink] = []
+
+    enum CodingKeys: String, CodingKey {
+        case title = "title"
+        case artistName = "artistName"
+        case nodesByUniqueId = "nodesByUniqueId"
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        title = try container.decode(String.self, forKey: .title)
+        artistName = try container.decode(String.self, forKey: .artistName)
+        nodesByUniqueId = try container.decode([String: SLNode].self, forKey: .nodesByUniqueId)
+    }
+
+    init(title: String, artistName: String, nodesByUniqueId: [String: SLNode]) {
+        self.title = title
+        self.artistName = artistName
+        self.nodesByUniqueId = nodesByUniqueId
+    }
 }
 
 struct SLLink: Decodable {
@@ -48,19 +66,21 @@ struct SLProvider {
     let url: String
 }
 
-enum SLError: Error {
-    case runtimeError(String)
+struct SLNode: Decodable {
+    let entity: String?
+    let listenUrl: String?
+    let listenAppUrl: String?
 }
 
 class SongLink {
     private var url: String? = nil
     private var providers: [SLProvider] = []
-    
+
     func load(_ url: String) -> Observable<[SLProvider]> {
         if providers.count > 0 {
             return Observable.just(providers)
         }
-        
+
         return requestData(url)
             .map({
                 self.url = $0.url
@@ -68,131 +88,166 @@ class SongLink {
             })
             .flatMap({ self.extractData($0) })
             .flatMap({ self.parseJson($0) })
+            .flatMap({ self.fillLinks($0) })
             .flatMap({ self.getProviders(data: $0) })
             .map({
                 self.providers = $0
                 return $0
             })
     }
-    
+
     private func parseJson(_ data: Data) -> Observable<SL> {
         return Observable.create() { observer in
             let disposable = Disposables.create()
-            
+
             guard let parsed = try? JSONDecoder().decode(SL.self, from: data) else {
-                observer.onError(SLError.runtimeError("Error: Couldn't decode data into SL"))
+                observer.onError(NSError(domain: "Error: Couldn't decode data into SL", code: 1, userInfo: nil))
                 observer.onCompleted()
                 return disposable
             }
-            
+
             observer.onNext(parsed)
             observer.onCompleted()
-            
+
             return disposable
         }
     }
-    
+
+    private func fillLinks(_ data: SL) -> Observable<SL> {
+        var slData = SLData(
+            title: data.songlink.title,
+            artistName: data.songlink.artistName,
+            nodesByUniqueId: data.songlink.nodesByUniqueId
+        )
+
+        data.songlink.nodesByUniqueId.forEach { key, value in
+            guard let entity: String = value.entity else { return }
+            guard let listenUrl: String = value.listenUrl else { return }
+
+            let listenAppUrl = value.listenAppUrl ?? ""
+            let provider = entity
+                .replacingOccurrences(of: "_SONG", with: "")
+                .replacingOccurrences(of: "_VIDEO", with: "")
+
+            slData.links.append(SLLink(
+                name: entity,
+                provider: provider,
+                url: listenUrl
+            ))
+
+            if listenAppUrl.count > 0 && provider == "YOUTUBE" {
+                slData.links.append(SLLink(
+                    name: "YOUTUBE_SONG",
+                    provider: "YOUTUBE",
+                    url: listenAppUrl
+                ))
+            }
+        }
+
+        return Observable.just(SL(songlink: slData))
+    }
+
     private func requestData(_ url: String) -> Observable<SLResponse> {
         return Observable.create() { observer in
             let targetUrl = URL(string: BASE_URL + url)
-            
+
             let task = URLSession.shared.dataTask(with: targetUrl!) { (data, response, error) in
                 guard let response = response as? HTTPURLResponse else {
-                    observer.onError(SLError.runtimeError("Cannot decode response"))
+                    observer.onError(NSError(domain: "Cannot decode response", code: 1, userInfo: nil))
                     observer.onCompleted()
                     return
                 }
-                
+
                 if response.statusCode != 200 {
                     let message = String(format: "Server returned %d status code", response.statusCode)
-                    observer.onError(SLError.runtimeError(message))
+                    observer.onError(NSError(domain: message, code: response.statusCode, userInfo: nil))
                     observer.onCompleted()
                     return
                 }
-                
+
                 if let data = data {
                     observer.onNext(SLResponse(url: response.url!.absoluteString, data: data))
                 } else if let error = error {
                     observer.onError(error)
                 }
-                
+
                 observer.onCompleted()
             }
-            
+
             task.resume()
-            
+
             return Disposables.create()
         }
     }
-    
+
     private func extractData(_ html: Data) -> Observable<Data> {
         return Observable.create() { observer in
             func error(_ error: Error) {
                 observer.onError(error)
                 observer.onCompleted()
             }
-            
+
             let disposable = Disposables.create()
-            
+
             guard let htmlString = String(data: html, encoding: .utf8) else {
-                error(SLError.runtimeError("HTML parsing error"))
+                error(NSError(domain: "HTML parsing error", code: 1, userInfo: nil))
                 return disposable
             }
-            
+
             guard let regex = try? NSRegularExpression(pattern: INITIAL_STATE_REGEXP) else {
-                error(SLError.runtimeError("NSRegularExpression creating error"))
+                error(NSError(domain: "NSRegularExpression creating error", code: 1, userInfo: nil))
                 return disposable
             }
-            
+
             let matches = regex.matches(
                 in: htmlString,
                 options: [],
                 range: NSMakeRange(0, htmlString.count)
             )
-            
+
             guard let match = matches.first, let range = Range(match.range(at: 1), in: htmlString) else {
-                error(SLError.runtimeError("Initial state not found"))
+                error(NSError(domain: "Initial state not found", code: 1, userInfo: nil))
                 return disposable
             }
-            
+
             guard let result = String(htmlString[range]).data(using: .utf8) else {
-                error(SLError.runtimeError("Cannot extract initialState"))
+                error(NSError(domain: "Cannot extract initialState", code: 1, userInfo: nil))
                 return disposable
             }
-            
+
             observer.onNext(result)
             observer.onCompleted()
-            
+
             return disposable
         }
     }
-    
+
     private func getProviders(data: SL) -> Observable<[SLProvider]> {
         return Observable.create { observer in
             var services: [SLProvider] = []
-            
+
             SERVICES.forEach() { item in
-                guard let remoteProvider = data.songlink.links.listen.first(where: { link in link.name == item.name }) else { return }
+                guard let remoteProvider = data.songlink.links.first(where: { link in link.name == item.name }) else { return }
                 let newProvider = SLProvider(
                     name: item.name,
                     label: item.label,
                     url: remoteProvider.url
                 )
-                
+
                 services.append(newProvider)
             }
-            
+
             let songlinkProvider = SLProvider(
                 name: SERVICES.last!.name,
                 label: SERVICES.last!.label,
                 url: self.url!
             )
-            
+
             services.append(songlinkProvider)
-            
+
             observer.onNext(services)
             observer.onCompleted()
-            
+
             return Disposables.create()
         }
     }
